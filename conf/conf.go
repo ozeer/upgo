0e8991f175c7053855e03412cab124f88a6c731b
zhouyang <zhouyang2021@163.com>
2023-05-22 12:13:55 +0800

    解决：因为zap改变了调用栈的深度，导致Viper在搜索配置文件时无法正确定位到相对路径的问题

--- conf/conf.go
+++ conf/conf.go
@@ -2,15 +2,20 @@ package conf
 
 import (
 	"fmt"
+	"path/filepath"
+	"runtime"
 
 	"github.com/spf13/viper"
 )
 
 func InitConfig() {
+	_, file, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(file)
+
 	// 读取配置文件
 	viper.SetConfigName("config")
 	viper.SetConfigType("yml")
-	viper.AddConfigPath("./conf/")
+	viper.AddConfigPath(dir)
 	err := viper.ReadInConfig()
 
 	if err != nil {
