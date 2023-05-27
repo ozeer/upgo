a303fdb3e0c7e12aabab808237b25a089562a701
zhouyang <zhouyang2021@163.com>
2023-05-28 01:19:15 +0800

    测试

--- conf/conf.go
+++ conf/conf.go
@@ -2,9 +2,11 @@ package conf
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 	"runtime"
 
+	"github.com/ozeer/upgo/service"
 	"github.com/spf13/viper"
 )
 
@@ -18,7 +20,15 @@ func InitConfig() {
 	viper.AddConfigPath(dir)
 	err := viper.ReadInConfig()
 
+	dir, errWd := os.Getwd()
+	if errWd != nil {
+		fmt.Println("获取当前目录失败:", err)
+		return
+	}
+
+	fmt.Println("当前所在目录:", dir)
 	fmt.Println("InitConfig dir: ", dir)
+	service.Command("ls")
 
 	if err != nil {
 		panic(fmt.Sprintf("Load config file error: %s", err.Error()))
